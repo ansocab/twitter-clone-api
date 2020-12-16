3095e36d52ff5d143fadfdeeb118d00fcd43f294
Ana Caballero <ana.caballero@gmx.de>
2020-12-16 13:56:01 +0100

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRouter = require("./routes/users");
 const messageRouter = require("./routes/messages");
+const cors = require("cors");
 
 const { PORT = 4000 } = process.env;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
